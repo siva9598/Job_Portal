88f9ecc31727c81d6437b9e440b6242925cd419c
siva9598 <sivasubramanian6996@gmail.com>
2021-11-28 15:55:26 +0530

    added signin  and signup functionality

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -7,6 +7,7 @@ const Op = db.Sequelize.Op;
 
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
+const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
 
 exports.signUp = (req, res) => {
   // Save User to Database
@@ -16,7 +17,8 @@ exports.signUp = (req, res) => {
     password: bcrypt.hashSync(req.body.password, 8),
     phone_number: req.body.phone_number
     
-  }.then(() => {res.send({ message: "User was registered successfully!" });})
+  }.then(() => {res.send({ message: "User was registered successfully!" });
+  res.redirect('/signIn')})
   )   
     .catch(err => {
         res.status(500).send({ message: err.message });
@@ -24,3 +26,32 @@ exports.signUp = (req, res) => {
     
   };
   
+  exports.signIn = (req,res) => {
+    const user = User.findOne()
+    const user = await User.findOne({ where: { email: req.body.email } });
+  }
+
+  exports.signIn = (req,res) => {
+    const { email, password } = req.body
+    const user = await User.findOne({ where: { email: email } })
+  
+    if (!user) {
+      return res.json({ status: 'error', error: 'Invalid username/password' })
+    }
+  
+    if (await bcrypt.compare(password, user.password)) {
+      // the username, password combination is successful
+  
+      const token = jwt.sign(
+        {
+          id: user.id,
+          email: user.email
+        },
+        JWT_SECRET
+      )
+  
+      return res.json({ status: 'ok', data: token })
+    }
+  
+    res.json({ status: 'error', error: 'Invalid username/password' })
+  }
