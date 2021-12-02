da3c9486d4a418444fd09b34c2d8522ce6530d2f
siva9598 <sivasubramanian6996@gmail.com>
2021-12-02 12:56:25 +0530

    add recuiter middleware

--- app/routes/job.routes.js
+++ app/routes/job.routes.js
@@ -2,14 +2,27 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const controller = require("../controllers/jobController");
+const auth = require("../middleware/authJWT");
+const recuiter_middleware = require("../middleware/verifyRecuiter");
 
-router.post("/job/create", auth.verifyToken, controller.createJob);
+router.post(
+  "/job/create",
+  auth.verifyToken,
+  recuiter_middleware.verifyRecuiter,
+  controller.createJob
+);
 router.patch(
   "/job/change_status",
   auth.verifyToken,
+  recuiter_middleware.verifyRecuiter,
   controller.changeJobstatus
 );
 router.get("/jobs", auth.verifyToken, controller.getAllJobs);
 router.post("/job/apply", auth.verifyToken, controller.applyForJob);
-router.get("/applicants", auth.verifyToken, controller.getAllApplicants);
+router.get(
+  "/applicants",
+  auth.verifyToken,
+  recuiter_middleware.verifyRecuiter,
+  controller.getAllApplicants
+);
 module.exports = router;
