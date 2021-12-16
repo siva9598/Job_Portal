e0a013c51fe58d7e813cf73a6219aef8baa78d63
siva9598 <sivasubramanian6996@gmail.com>
2021-12-16 22:22:13 +0530

    added crud operations for user profile

--- app/controllers/profileController.js
+++ app/controllers/profileController.js
@@ -25,3 +25,50 @@ exports.createUserProfile = async (req, res) => {
       });
   }
 };
+
+exports.getUserProfile = async (req, res) => {
+  const user_id = req.userId;
+  console.log("user id: " + user_id);
+  await UserProfile.findOne({
+    where: {
+      userId: user_id,
+    },
+  })
+    .then((user_profile) => {
+      return res.json(user_profile);
+    })
+    .catch((err) => {
+      res.status(400).send(err.message);
+    });
+};
+
+exports.changeUserProfile = async (req, res) => {
+  if (!req.userId) {
+    {
+      res.status(400).send({
+        msg: "Please send the valid data",
+      });
+    }
+  } else {
+    UserProfile.update(
+      {
+        current_location: req.body.current_location,
+        current_role: req.body.current_role,
+        current_salary: req.body.current_salary,
+        total_work_experience_in_years: req.body.total_work_experience_in_years,
+        resume_link: req.body.resume_link,
+        linkedIn_link: req.body.linkedIn_link,
+        github_link: req.body.github_link,
+      },
+      {
+        where: {
+          userId: req.userId,
+        },
+      }
+    )
+      .then(() => res.status(200).send("User profile updated successfully"))
+      .catch((err) => {
+        res.status(400).send(err.message);
+      });
+  }
+};
--- app/routes/user_profile.routes.js
+++ app/routes/user_profile.routes.js
@@ -5,5 +5,7 @@ const auth = require("../middleware/authJWT");
 const controller = require("../controllers/profileController");
 
 router.post("/create", auth.verifyToken, controller.createUserProfile);
+router.get("/get", auth.verifyToken, controller.getUserProfile);
+router.patch("/update", auth.verifyToken, controller.changeUserProfile);
 
 module.exports = router;
