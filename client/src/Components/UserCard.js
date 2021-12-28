1ba6ba395a8e781cff46a08bffae5f8fcea3548c
siva9598 <sivasubramanian6996@gmail.com>
2021-12-28 10:35:05 +0530

    style user card

--- client/src/Components/UserCard.js
+++ client/src/Components/UserCard.js
@@ -4,22 +4,33 @@ import "./JobCard.css";
 import { Button } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import { applyForJob } from "../Helpers/JobHelper";
-
+import "./UserCard.css";
 const UserCard = (props) => {
   const [appliedAlready, setAppliedAlready] = useState(0);
   return (
-    <div className="job-card-exterior">
-      <div className="job-card">
-        <div className="job-details">
+    <div className="user-card-exterior">
+      <div className="user-card">
+        <div className="user-details">
           <h6>Name: {props.name}</h6>
-          <h6>salary{props.current_salary}</h6>
-          <h6>role {props.current_role}</h6>
-          <h6>total work exp{props.total_work_experience_in_years}</h6>
-          <h6> resume: {props.resume_link}</h6>
-          <h6>github {props.github_link}</h6>
+          <h6>Salary {props.current_salary}</h6>
+          <h6>Role: {props.current_role}</h6>
+          <h6>Total work exp: {props.total_work_experience_in_years}</h6>
+          <h6> Resume: {props.resume_link}</h6>
+          <h6>Github: {props.github_link}</h6>
         </div>
-
-        <h6>{props.status}</h6>
+        <Button
+          className="btn btn-primary apply-button"
+          //   onClick={applyJobAsUser}
+        >
+          shortlist
+        </Button>
+        <Button
+          className="btn btn-primary apply-button"
+          //   onClick={applyJobAsUser}
+        >
+          reject
+        </Button>
+        {/* <h6>{props.status}</h6> */}
       </div>
     </div>
   );
